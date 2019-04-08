9fc015092fde427a387ab6a5b77839cb1455d2d2
Kassai Yevhenii <ekleito@gmail.com>
2019-04-08 19:54:37 +0300

    08/04

---
+++ test/src/js/script.js
@@ -0,0 +1,21 @@
+;(function IIFE(){
+
+// console.log ( 'Hello' );
+
+// const text = 'I love Borsch';
+
+// document.write (text);
+
+// const textField = document.getElementById('textField');
+
+// console.dir(textField);
+
+// const text = 'I love Borsch';
+
+// textField.innerText = text;
+
+const random = Math.floor(Math.random() * 100000000000);
+
+console.log(random);
+
+})();
